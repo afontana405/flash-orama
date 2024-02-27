741fd03e6785e2c93e60c1ea42cc8e6112c486fc
misschootemup <misschootemup@gmail.com>
2024-02-26 18:32:22 -0600

    Styling and such

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,9 +39,9 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
+      <div className="flex-column test justify-flex-start min-100-vh">
         <Header />
-        <div className="container">
+        <div className="container outlet-wrapper">
           <Outlet />
         </div>
         <Footer />
--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -8,30 +8,30 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+    <header className=" navbar text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
             <h1 className="m-0">Flash-orama</h1>
           </Link>
-          <p className="m-0">Success in a Flash</p>
+          <p className="m-0">Pass in a Flash</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/me">
+              <Link className="btn-background-link m-2" to="/me">
                 {Auth.getProfile().data.username}'s profile
               </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+              <button className="btn-background m-2" onClick={logout}>
                 Logout
               </button>
             </>
           ) : (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
+              <Link className="btn-background-link  m-2" to="/login">
                 Login
               </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
+              <Link className="btn-background-link  m-2" to="/signup">
                 Signup
               </Link>
             </>
